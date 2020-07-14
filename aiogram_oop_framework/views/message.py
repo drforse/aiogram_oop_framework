5fa3bb55cea16dc037f927c95993174920337334
drforse <george.lifeslice@gmail.com>
2020-07-14 03:17:13 +0100

    start creating docs

--- aiogram_oop_framework/views/message.py
+++ aiogram_oop_framework/views/message.py
@@ -6,16 +6,34 @@ from .base import BaseView
 
 
 class MessageView(BaseView):
+    """View for updates of type "message"
+
+    Attributes:
+        custom_filters (list): Custom filters (for ex.: [lambda m: m.reply_to_message]).
+
+        commands (list): List of commands (for ex.: ['start', 'help']).
+
+        regexp (str): Regexp string for matching message text/caption (for ex.: r'^hello$')
+
+        content_types (list): List of content_type's of message (for ex.: ['text', 'photo'])
+
+        state (Callable): Function, which returns a State object of aiogram or "*"
+
+        run_task (bool): Run callback in task (no wait results)
+
+        register_kwargs (dict): Kwargs, which you would add in @dp.message_handler in fresh aiogram
+
+
+    You may found more info about attributes in aiogram's docs on Dispatcher.message_handler or you may not, depends on aiogram's docs.
+
+    """
     @classmethod
     async def execute(cls, m: types.Message, state: FSMContext = None, **kwargs):
         pass
 
     @classmethod
     def register(cls, dp: Dispatcher):
-        """
-
-        Make sure you don't want to use a more high-level like Command or Photo view instead
-        """
+        """Make sure you don't want to use a more high-level view like CommandView or PhotoView instead"""
         callback = cls.execute
         kwargs = cls.register_kwargs if cls.register_kwargs else {}
         custom_filters = cls.custom_filters if cls.custom_filters else []
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+.. aiogram_oop_framework documentation master file, created by
+   sphinx-quickstart on Tue Jul 14 01:06:40 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to aiogram_oop_framework's documentation!
+=================================================
+
+.. automodule:: aiogram_oop_framework.views.message
+    :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
