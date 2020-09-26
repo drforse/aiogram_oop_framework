397db8597e0dcf810379701a103be668ac1ff9a3
drforse <george.lifeslice@gmail.com>
2020-09-26 18:32:17 +0100

    import missing views

--- aiogram_oop_framework/views/__init__.py
+++ aiogram_oop_framework/views/__init__.py
@@ -1,6 +1,13 @@
 from .callback_query import CallbackQueryView
 from .inline_query import InlineQueryView
 from .message import MessageView
+from .channel_post_view import ChannelPostView
+from .chosen_inline_result import ChosenInlineResultView
+from .edited_channel_post_view import EditedChannelPostView
+from .edited_message_view import EditedMessageView
+from .poll import PollView
+from .poll_answer import PollAnswerView
+from .shipping_query import ShippingQueryView
 
 from .content_types_views import *
 from .custom_views import *
